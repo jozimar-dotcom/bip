04290bb2c4789c1c85964dc5aee3a45b7f25778e
jozimar-dotcom <jozimar@infolondrina.com.br>
2025-09-02 12:29:54 -0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "jztecc30_envios";
+$user = "jztecc30_envios";
+$pass = "Imperio72##";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass); // trocado $pdo por $conn
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+}
+?>
